da061597a3cb81dd5c181b1616a979facafaccf1
Camila Machado <sra.mac18@gmail.com>
2023-04-25 19:07:30 -0300

    Code responsive css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
           !wallet?
           (
             <div className="col-10 col-sm-8 col-lg-6 py-3">
-            <img className="global-background" src="/image/global.png" alt="happy people" />
+            <img className="global-background" src="/image/global.png" alt="world" />
               
               {/* <img className="d-block mx-lg-auto img-fluid" width="700" height="500" src="https://images.unsplash.com/photo-1520694478166-daaaaec95b69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="happy people" /> */}
             </div>
@@ -35,27 +35,27 @@ export default function Home() {
               <p className="mb-3">Be Welcome {wallet}</p>
               <p className="mb-3">What do you want to do? </p>
               <div className="col-12">
-                <p><Link href="/donate" className='btn btn-primary col-6 p-3'>Quero fazer uma doação</Link></p>
-                <p><Link href="/create" className='btn btn-secondary col-6 p-3'>Quero criar uma campanha</Link></p>
+                <p><Link href="/donate" className='btn btn-primary col-6 p-3'>I want to make a donation.</Link></p>
+                <p><Link href="/create" className='btn btn-secondary col-6 p-3'>I wanna to create a campaign.</Link></p>
               </div>
 
             </div>
           )
         }
           <div className="col-lg-8 py-5">
-          <h1 className="fw-bold mb-3 py-5">Donate Crypto</h1>          
+            <h1 className="fw-bold mb-3 py-5">Donate Crypto</h1>          
           </div>
           <div className="col-lg-6">
-            <p>Sua plataforma descentralizada de doações.</p>
-            <p className="mb-3">Autentique-se com sua carteira, crie sua campanha ou doe para campanhas existentes.</p>
+            <p>Your decentralized donation platform.</p>
+            <p className="mb-3">Log in with wallet, create your campaign or donate for existing campaign.</p>
             {!wallet ?(
               
               <div className='flex align-items-center button-start'>
                 <button className="btn btn-primary btn-lg px-4 me-md-2 col-8" type='button' onClick={btnLoginClick}>
                   <img src="/metamask.svg" width={64} className='me-3' alt="Clique no botão para doar" />
-                  Conectar com a Metamask
+                  Connect with Metamask
                 </button>
-                <img src="/image/icon-button.png" className='icon_button col-4 px-4 me-md-2' alt="" />
+                <img src="/image/icon-button.png" className='icon_button col-4 px-4 me-md-2' alt="donating hand" />
               </div>
             ):<></>
             }
